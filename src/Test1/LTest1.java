11d4241a7df0f2fbee9914379afa63354f5951e4
Ajit77670 <Ajit77670@gmail.com>
2023-01-02 23:27:43 +0530

    Creating the GitPractise_2 Repo

---
+++ src/Test1/LTest1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test1;
+
+/**
+ *
+ * @author Ajith Kumar
+ */
+public class LTest1 {
+    
+    String Display(){
+    
+        return "Happy new Year";
+    
+    }
+    
+}
