a7f33b5a02c8b3a0345bd8d6f4e915090dc1d851
AjitQA <62891736+Ajit77670@users.noreply.github.com>
2023-01-04 00:04:59 +0530

    Create Stash_wrk.java

---
+++ src/Page1/Stash_wrk.java
@@ -0,0 +1,10 @@
+package Page1;
+
+public class Stash_wrk {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hi This is stash wrk at remote in progress");
+    }
+    
+}
