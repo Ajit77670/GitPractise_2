310eeae1b2df48785c6f2342fd4776762fcf7ad6
Ajit77670 <Ajit77670@gmail.com>
2023-01-03 10:23:01 +0530

    Tag commit

---
+++ src/Page1/Tags.java
@@ -0,0 +1,15 @@
+
+package Page1;
+
+
+public class Tags {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("V 1.0");
+        System.out.println("V 2.0");
+        System.out.println("V 3.0");
+        
+    }
+    
+}
