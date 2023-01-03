42cbdb684a3be998aea782687fe9ce4c07c06212
Ajit77670 <Ajit77670@gmail.com>
2023-01-03 17:05:05 +0530

    My second cherry pick commit

--- src/Page1/CherryPick.java
+++ src/Page1/CherryPick.java
@@ -6,6 +6,7 @@ public class CherryPick {
     public static void main(String[] args) {
         
         System.out.println("Hi This is Cheery Pick Tutorial");
+        System.out.println("Hi there");
     }
     
 }
