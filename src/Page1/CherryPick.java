1708510f60692d947e6970941ee0a8b4286e4e46
Ajit77670 <Ajit77670@gmail.com>
2023-01-03 17:02:24 +0530

    MyFirsy cheery commit

---
+++ src/Page1/CherryPick.java
@@ -0,0 +1,11 @@
+
+package Page1;
+
+public class CherryPick {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hi This is Cheery Pick Tutorial");
+    }
+    
+}
