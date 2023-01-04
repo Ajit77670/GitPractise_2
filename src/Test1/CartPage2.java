dd69594d59f44cefee365430bb9614644ab754c9
AjitQA <62891736+Ajit77670@users.noreply.github.com>
2023-01-04 12:37:04 +0530

    Create CartPage2

---
+++ src/Test1/CartPage2.java
@@ -0,0 +1,20 @@
+
+package Test1;
+
+public class CartPage2 {
+    
+    public class java {
+    
+    public void cart(){
+        
+        String strDev2=" cart Page Dev2";
+        int cart =100;
+        int sal =2500;
+        int age =30;
+        
+        System.out.println(strDev2);
+        
+        
+    }
+    
+}
