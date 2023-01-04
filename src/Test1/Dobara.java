cbc0d1c5e0c268123a8c69c9f96902c72359155a
AjitQA <62891736+Ajit77670@users.noreply.github.com>
2023-01-04 13:58:17 +0530

    Dobara by Dev2

---
+++ src/Test1/Dobara.java
@@ -0,0 +1,13 @@
+
+package Test1;
+
+
+public class Dobara {
+    
+    public static void main(String[] args) {
+        
+        String Dev2 = "Dobara by Dev2";
+        int salary = 15;
+        int age=15;
+    }
+}
