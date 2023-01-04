9cb8eb317289ae44331fde9d26160d56727c8cfb
Ajit77670 <Ajit77670@gmail.com>
2023-01-04 13:54:07 +0530

    update

---
+++ src/Test1/CartPage.java
@@ -0,0 +1,15 @@
+
+package Test1;
+
+
+public class CartPage {
+    
+    public static void main(String[] args) {
+        
+        String Dev1= "Dev1 CART PAGE";
+        int age=25;
+        int salary =2500;
+        System.out.println(Dev1);
+    }
+    
+}
