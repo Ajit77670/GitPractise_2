81b7f926f6f0d0ae7a292e052d15fc315e4c769e
Ajit77670 <Ajit77670@gmail.com>
2023-01-04 14:35:13 +0530

    delete2

--- src/Test1/Dobara.java
+++ src/Test1/Dobara.java
@@ -6,8 +6,10 @@ public class Dobara {
     
     public static void main(String[] args) {
         
-        String Dev2 = "Dobara by Dev2";
-        int salary = 15;
-        int age=15;
+
+        String Dev1 = "Dobara by Dev1";
+        int salary = 10;
+        int age=10;
+
     }
 }
