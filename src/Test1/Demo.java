6bb0c886eafebc0d97fb59cf9bc8a78db2f5f14f
Ajit77670 <Ajit77670@gmail.com>
2023-01-04 11:03:37 +0530

    Demo First Commit

---
+++ src/Test1/Demo.java
@@ -0,0 +1,19 @@
+
+package Test1;
+
+
+public class Demo {
+    
+    public void show(){
+        
+      String str1 = "Hello Geeks";
+      int n= 100;
+      int k = 200;
+      int j = n+k;
+      
+        
+        
+    }
+    }
+    
+
