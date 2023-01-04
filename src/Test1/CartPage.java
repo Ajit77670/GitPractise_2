b3dc92e935e412dcc93c7e0771d29478a3e5d376
Ajit77670 <Ajit77670@gmail.com>
2023-01-04 13:51:11 +0530

    updated

--- src/Test1/CartPage.java
+++ src/Test1/CartPage.java
@@ -6,10 +6,12 @@ public class CartPage {
     
     public static void main(String[] args) {
         
-        String Dev2= "Dev2 CART PAGE";
+
+        String Dev1= "Dev1 CART PAGE";
         int age=25;
-        int salary =2500;
-        System.out.println(Dev2);
+        int salary =4500;
+        System.out.println(Dev1);
+
     }
     
 }
