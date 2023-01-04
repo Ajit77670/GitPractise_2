2d5167592e5203792490058f28b2d79acdaca77d
Ajit77670 <Ajit77670@gmail.com>
2023-01-04 11:38:05 +0530

    Demo2 Local commit

---
+++ src/Demo2/java.java
@@ -0,0 +1,16 @@
+
+package Demo2;
+
+
+public class java {
+    
+    public void show(){
+    
+    String str1 ="Hi";
+    String str2="Hello";
+    int n=10;
+    int age =12;
+    int sal=15;
+    }
+    
+}
